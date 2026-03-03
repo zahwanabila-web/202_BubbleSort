bbdcc79b6fec3665a2068da925674063561abe59
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-03 14:57:48 +0700

    display

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -58,3 +58,26 @@ void bubbleSortArray()
 
 } while (pass <= n - 1);
 }
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen Array yang telah tersusun"<< endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
