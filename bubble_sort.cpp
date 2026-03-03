174e0af61fb436a58b02ee730a39f7c3e1b3ab42
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-03 14:47:38 +0700

    bubbleSortArray

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -24,4 +24,37 @@ void input()
     cout <<"Masukkan Elemen Array" << endl;
     cout << "===================" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+
+    do
+    {
+        for (int j = 0; j <= n - 1 pass; j++)
+    {
+        if (arr[j]> arr[j + 1])
+        {
+             int temp;
+            temp= arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }   
+    }
+    pass += 1;
+
+    cout << "\nPass " << pass - 1 << "; ";
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+    cout << endl;
+
+} while (pass <= n - 1);
 }
