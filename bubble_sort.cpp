556fbc934f4ab16bb974948957232600604df979
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-03 14:21:11 +0700

    input

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -4,3 +4,24 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "===================" << endl;
+    cout <<"Masukkan Elemen Array" << endl;
+    cout << "===================" << endl;
+
+}
